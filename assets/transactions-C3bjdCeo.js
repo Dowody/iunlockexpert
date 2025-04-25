388c2917aca7078020b1044ca58978840aa658d6
Dowody <54286055+Dowody@users.noreply.github.com>
2025-04-25 17:35:12 +0300

    Updates

---
+++ assets/transactions-C3bjdCeo.js
@@ -0,0 +1,16 @@
+import{i as f,a as m,x as a}from"./index-D5QqPFni.js";import{c as p}from"./if-defined-BvjKv9s7.js";import"./index-Co-Uhxg0.js";import"./index-Do6wI9eK.js";import"./index-D1W2eIx2.js";import"./index-XBlUAFK2.js";import"./index-BN42EdjE.js";import"./index-CPxxPYrc.js";const d=f`
+  :host > wui-flex:first-child {
+    height: 500px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: none;
+  }
+
+  :host > wui-flex:first-child::-webkit-scrollbar {
+    display: none;
+  }
+`;var u=function(o,t,i,r){var n=arguments.length,e=n<3?t:r===null?r=Object.getOwnPropertyDescriptor(t,i):r,l;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")e=Reflect.decorate(o,t,i,r);else for(var c=o.length-1;c>=0;c--)(l=o[c])&&(e=(n<3?l(e):n>3?l(t,i,e):l(t,i))||e);return n>3&&e&&Object.defineProperty(t,i,e),e};let s=class extends m{render(){return a`
+      <wui-flex flexDirection="column" .padding=${["0","m","m","m"]} gap="s">
+        <w3m-activity-list page="activity"></w3m-activity-list>
+      </wui-flex>
+    `}};s.styles=d;s=u([p("w3m-transactions-view")],s);export{s as W3mTransactionsView};
