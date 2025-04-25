f7dc9de8ac46b10733e1fec03356f9b11ca21b52
Dowody <54286055+Dowody@users.noreply.github.com>
2025-04-25 18:01:25 +0300

    1.2.3

--- src/components/config/index.tsx
+++ src/components/config/index.tsx
@@ -2,8 +2,9 @@ import { mainnet, arbitrum, sepolia } from '@reown/appkit/networks'
 import type { AppKitNetwork } from '@reown/appkit/networks'
 import { EthersAdapter } from '@reown/appkit-adapter-ethers'
 
+
 // Get projectId from https://cloud.reown.com
-export const projectId = import.meta.env.VITE_PROJECT_ID || "acf46e0cc4d322acb6d86bb030902765" // this is a public projectId only to use on localhost
+export const projectId = import.meta.env.VITE_PROJECT_ID || "123" // this is a public projectId only to use on localhost
 
 if (!projectId) {
   throw new Error('Project ID is not defined')
@@ -11,8 +12,8 @@ if (!projectId) {
 
 // Create a metadata object - optional
 export const metadata = {
-  name: 'AppKit',
-  description: 'AppKit',
+  name: 'iUnlockExpert',
+  description: 'iUnlockExpert Website',
   url: 'https://dowody.github.io/iunlockexpert/', // origin must match your domain & subdomain
   icons: ['https://avatars.githubusercontent.com/u/179229932']
 }
